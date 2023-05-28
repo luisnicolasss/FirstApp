3c307e1ad9c620abf6f2e8ee0141adfef4e1c6af
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-05-28 11:48:01 -0300

    Inicio TODO App

--- app/src/main/java/com/example/androidmaster/MenuActivity.kt
+++ app/src/main/java/com/example/androidmaster/MenuActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import com.example.androidmaster.firstapp.FirstAppActivity
 import com.example.androidmaster.imccalculator.ImcCalculatorActivity
+import com.example.androidmaster.todoapp.TodoActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +14,16 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
         val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
         val btnImcApp = findViewById<Button>(R.id.btnIMCApp)
+        val btnTODO = findViewById<Button>(R.id.btnTODO)
         btnSaludApp.setOnClickListener { navigateToSaludApp() }
         btnImcApp.setOnClickListener { navigateToImcApp() }
+        btnTODO.setOnClickListener { navigateToTodoApp() }
 
+    }
+
+    private fun navigateToTodoApp() {
+      val intent = Intent(this, TodoActivity::class.java)
+      startActivity(intent)
     }
 
     private fun navigateToImcApp() {
