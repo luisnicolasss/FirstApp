9773241296fe60863dc77fd3d655a8860258844b
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-05-29 12:03:06 -0300

    Vista de categorias con Adapter y ViewHolder

--- app/src/main/java/com/example/androidmaster/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/androidmaster/todoapp/TodoActivity.kt
@@ -2,11 +2,39 @@ package com.example.androidmaster.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.androidmaster.R
 
 class TodoActivity : AppCompatActivity() {
+
+
+   private val categories = listOf(
+       TaskCategory.Business,
+       TaskCategory.Personal,
+       TaskCategory.Other
+   )
+
+    private lateinit var categoriesAdapter: CategoriesAdapter
+    private lateinit var rvCategories: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
+        initComponent()
+        initUI()
+    }
+
+
+
+    private fun initComponent() {
+        rvCategories = findViewById(R.id.rvCategories)
+    }
+
+    private fun initUI() {
+       categoriesAdapter = CategoriesAdapter(categories)
+       rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+       rvCategories.adapter = categoriesAdapter
     }
 }
