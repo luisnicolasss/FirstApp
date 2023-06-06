998620e6eec9c0bda21d013a848c06c90f73dbd9
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-06-06 15:20:03 -0300

    Vista de Dialog

--- app/src/main/java/com/example/androidmaster/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/androidmaster/todoapp/TodoActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidmaster.todoapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -8,6 +9,7 @@ import com.example.androidmaster.R
 import com.example.androidmaster.mutableList
 import com.example.androidmaster.todoapp.adapters.CategoriesAdapter
 import com.example.androidmaster.todoapp.adapters.TasksAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TodoActivity : AppCompatActivity() {
 
@@ -29,18 +31,31 @@ class TodoActivity : AppCompatActivity() {
     private lateinit var tasksAdapter: TasksAdapter
     private lateinit var rvCategories: RecyclerView
     private lateinit var rvTasks: RecyclerView
+    private lateinit var fabAddTask: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
         initComponent()
         initUI()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        fabAddTask.setOnClickListener { showDialog() }
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
+        dialog.show()
     }
 
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
         rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabAddTask)
     }
 
     private fun initUI() {
