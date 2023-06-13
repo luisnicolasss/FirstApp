bd07b3e5ae5c4eed4205caf35412b3f64e5bde7a
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-06-13 11:51:14 -0300

    initUI

--- app/src/main/java/com/example/androidmaster/superheroapp/SuperHeroListActivity.kt
+++ app/src/main/java/com/example/androidmaster/superheroapp/SuperHeroListActivity.kt
@@ -2,11 +2,28 @@ package com.example.androidmaster.superheroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
 import com.example.androidmaster.R
+import com.example.androidmaster.databinding.ActivitySuperHeroListBinding
 
 class SuperHeroListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySuperHeroListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_super_hero_list)
+        binding = ActivitySuperHeroListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun onQueryTextChange(newText: String?) = false
+
+        })
     }
 }
