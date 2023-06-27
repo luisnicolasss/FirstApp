74996ef817fb438aafbf6f9fd68f415c94d64625
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-06-27 13:00:58 -0300

    Interfaz de Settings

--- app/src/main/java/com/example/androidmaster/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/androidmaster/settings/SettingsActivity.kt
@@ -1,9 +1,16 @@
 package com.example.androidmaster.settings
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.example.androidmaster.R
 import com.example.androidmaster.databinding.ActivitySettingsBinding
+
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class SettingsActivity : AppCompatActivity() {
 
