1f80d1911e38cab13a1047a21eade3deaa85ca1e
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-06-27 14:22:22 -0300

    Guardar valores con DataStore

--- app/src/main/java/com/example/androidmaster/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/androidmaster/settings/SettingsActivity.kt
@@ -5,14 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.example.androidmaster.R
 import com.example.androidmaster.databinding.ActivitySettingsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class SettingsActivity : AppCompatActivity() {
+
+    companion object {
+        const val VOLUME_LVL = "volumen_lvl"
+        const val KEY_BLUETOOTH = "key_bluetooth"
+        const val KEY_VIBRATION = "key_vibration"
+        const val KEY_DAR_MODE = "key_dark_mode"
+    }
 
     private lateinit var binding: ActivitySettingsBinding
 
@@ -20,5 +33,44 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        binding.rsVolume.addOnChangeListener { _, value, _ ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveVolume(value.toInt())
+            }
+        }
+
+        binding.switchBluetooth.setOnCheckedChangeListener { _, value ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveOptions(value, KEY_BLUETOOTH)
+            }
+        }
+
+        binding.switchVibration.setOnCheckedChangeListener { _, value ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveOptions(value, KEY_VIBRATION)
+            }
+        }
+
+        binding.switchDarkMode.setOnCheckedChangeListener { _, value ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveOptions(value, KEY_DAR_MODE)
+            }
+        }
+    }
+
+    private suspend fun saveVolume(value:Int){
+        dataStore.edit {
+            preferences -> preferences[intPreferencesKey(VOLUME_LVL)] = value
+        }
+    }
+
+    private suspend fun saveOptions(value:Boolean, key:String){
+        dataStore.edit {
+                preferences -> preferences[booleanPreferencesKey(key)] = value
+        }
     }
 }
